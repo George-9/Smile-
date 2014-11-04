6558b9cc4d00fec95d82f2be4cc828e62cc1b3c9
George Ashton <George@Georges-MacBook-Pro.local>
2014-11-04 20:41:04 +0000

    Initial Commit

---
+++ Smile+/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Smile+
+//
+//  Created by George Ashton on 04/11/2014.
+//  Copyright (c) 2014 George Ashton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Smile+/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Smile+
+//
+//  Created by George Ashton on 04/11/2014.
+//  Copyright (c) 2014 George Ashton. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
